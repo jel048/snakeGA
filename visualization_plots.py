cfd617f74203dff333a78e0096df150421373d38
Rampestrek1 <jel048@post.uit.no>
2024-11-08 13:39:17 +0100

    fixed on some plots

--- visualization_plots.py
+++ visualization_plots.py
@@ -1,28 +1,28 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-version = 'v15.1'
+version = 'v17.1'
 
 #Load the CSV file into a DataFrame
 df =pd.read_csv(f'model_logs/{version}.csv')
 
-# Plot 1: Length Mean vs Iteration
+#Plot 1: Length Mean vs Iteration
 plt.figure(figsize=(10, 6))
 plt.plot(df['iteration'], df['length_mean'], color='b', label='Length Mean')
 plt.xlabel('Iteration')
 plt.ylabel('Length Mean')
-plt.title('Iteration vs Length Mean')
+plt.title(f'Mean length of snake during training, version: {version}')
 plt.grid(True)
 plt.legend()
-plt.savefig(f'images/length_mean_vs_iteration_{version}.png')  # Save the plot as an image
-plt.clf()  # Clear the current figure for the next plot
+plt.savefig(f'images/length_mean_vs_iteration_{version}.png')  #save the plot as an image
+plt.clf()  #clear the figure for the next plot
 
-# Plot 2: Reward Mean vs Iteration
+#Plot 2: Reward Mean vs Iteration
 plt.figure(figsize=(10, 6))
 plt.plot(df['iteration'], df['reward_mean'], color='g', label='Reward Mean')
 plt.xlabel('Iteration')
 plt.ylabel('Reward Mean')
-plt.title('Iteration vs Reward Mean')
+plt.title(f'Mean rewards during training, version: {version}')
 plt.grid(True)
 plt.legend()
-plt.savefig(f'images/reward_mean_vs_iteration_{version}.png')  # Save the plot as an image
+plt.savefig(f'images/reward_mean_vs_iteration_{version}.png')  #saave the plot as an image
